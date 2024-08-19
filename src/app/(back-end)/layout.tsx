1cacf904807fe5c36fe4804d8ae4486a5882ee29
Kisakye Moses <111237257+mosespace@users.noreply.github.com>
2024-08-19 16:09:21 +0300

    feat: initial commit

--- src/app/(back-end)/layout.tsx
+++ src/app/(back-end)/layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/connections/auth";
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -16,7 +16,12 @@ export default async function DashboardLayout({
   if (!session) {
     redirect("/login");
   }
+
   const user: User = session?.user;
+
+  if (user.role !== "ADMIN") {
+    return notFound();
+  }
 
   return (
     <div className='w-full md:fixed md:inset-x-0 grid min-h-screen md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]'>
